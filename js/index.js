0fbc12a4d56f2169336575ba8c3c67307814c12f
Patrick Kim <pkim050@gmail.com>
2019-06-09 14:51:11 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function showRepositories() {
   //this is set to the XMLHttpRequest object that fired the event
   console.log(this.responseText);
 }
- 
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
